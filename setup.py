29dfd453dd708c71dd0777ec7ee55bde9ce7dd7e
Christopher Burns <cburns@berkeley.edu>
2009-09-16 23:04:05 -0700

    Add basic setup.py to install rst2blog.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Lacquer',
+      version='0.1',
+      description='Thin layer of convenience on top of useful Python libraries.',
+      author='Fernando Perez',
+      url='http://github.com/fperez/lacquer',
+      scripts=['./lacquer/rst2blog',]
+     )
+
