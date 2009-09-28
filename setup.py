7cd322593d0c8b01b090e12618a88403af69b241
Ondrej Certik <ondrej@certik.cz>
2009-09-28 16:35:58 -0700

    latex2rst added to setup.py
    Also the whitespace in setup.py was normalized to 4 spaces for an indent.

--- setup.py
+++ setup.py
@@ -3,10 +3,13 @@
 from distutils.core import setup
 
 setup(name='Lacquer',
-      version='0.1',
-      description='Thin layer of convenience on top of useful Python libraries.',
-      author='Fernando Perez',
-      url='http://github.com/fperez/lacquer',
-      scripts=['./lacquer/rst2blog',]
+    version='0.1',
+    description='Thin layer of convenience on top of useful Python libraries.',
+    author='Fernando Perez',
+    url='http://github.com/fperez/lacquer',
+    scripts=[
+        './lacquer/rst2blog',
+        './lacquer/latex2rst',
+        ]
      )
 
